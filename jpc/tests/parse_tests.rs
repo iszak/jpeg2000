86f79ae5aeaaa67a0d3167ad4d03e23fb241f0ea
Brad Hards <bradh@frogmouth.net>
2025-11-05 21:19:48 +1100

    fix(jpc): correct result of SIZ precision
    The parsing was OK, but the accessor used the wrong value.
    
    Includes a new test case, which will be extended to check the other parts
    of the header and tiles.

---
+++ jpc/tests/parse_tests.rs
@@ -0,0 +1,44 @@
+use std::{fs::File, io::BufReader, path::Path};
+
+use jpc::{decode_jpc};
+
+#[test]
+fn test_blue() {
+    let filename = "blue.j2k";
+    let path = Path::new(env!("CARGO_MANIFEST_DIR"))
+        .join("tests")
+        .join(filename);
+    let file = File::open(path).expect("file should exist");
+    let mut reader = BufReader::new(file);
+    let result = decode_jpc(&mut reader);
+    assert!(result.is_ok());
+    let codestream = result.unwrap();
+    assert_eq!(codestream.length(), 0);
+    assert_eq!(codestream.offset(), 0);
+
+    let header = codestream.header();
+    println!("\nheader: {header:?}");
+
+    let siz = header.image_and_tile_size_marker_segment();
+    assert_eq!(siz.reference_grid_width(), 128);
+    assert_eq!(siz.reference_grid_height(), 64);
+    assert_eq!(siz.image_horizontal_offset(), 0);
+    assert_eq!(siz.image_vertical_offset(), 0);
+    assert_eq!(siz.offset(), 4);
+    assert_eq!(siz.length(), 47);
+    assert_eq!(siz.decoder_capabilities(), 0);
+    assert_eq!(siz.image_horizontal_offset(), 0);
+    assert_eq!(siz.image_vertical_offset(), 0);
+    assert_eq!(siz.reference_tile_width(), 128);
+    assert_eq!(siz.reference_tile_height(), 64);
+    assert_eq!(siz.no_components(), 3);
+    assert_eq!(siz.precision(0).unwrap(), 7);
+    assert_eq!(siz.precision(1).unwrap(), 7);
+    assert_eq!(siz.precision(2).unwrap(), 7);
+    assert_eq!(siz.horizontal_separation(0).unwrap(), 1);
+    assert_eq!(siz.horizontal_separation(1).unwrap(), 1);
+    assert_eq!(siz.horizontal_separation(2).unwrap(), 1);
+    assert_eq!(siz.vertical_separation(0).unwrap(), 1);
+    assert_eq!(siz.vertical_separation(1).unwrap(), 1);
+    assert_eq!(siz.vertical_separation(2).unwrap(), 1);
+}

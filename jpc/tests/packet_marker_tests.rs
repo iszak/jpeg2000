d20f68543374f6c4c328899a7254d3ed63cbd47f
Iszak Bryan <38895+iszak@users.noreply.github.com>
2025-11-19 00:54:46 +0000

    jpc: assert code-block width/height in test

--- jpc/tests/packet_marker_tests.rs
+++ jpc/tests/packet_marker_tests.rs
@@ -65,8 +65,8 @@ fn test_eph() {
     );
     // SPcod
     assert_eq!(cod.coding_style_parameters().no_decomposition_levels(), 0);
-    // assert_eq!(cod.coding_style_parameters().code_block_width(), 64);
-    // assert_eq!(cod.coding_style_parameters().code_block_height(), 64);
+    assert_eq!(cod.coding_style_parameters().code_block_width(), 64);
+    assert_eq!(cod.coding_style_parameters().code_block_height(), 64);
     assert_eq!(cod.coding_style_parameters().code_block_style(), 0);
     assert_eq!(
         cod.coding_style_parameters().coding_block_styles(),
